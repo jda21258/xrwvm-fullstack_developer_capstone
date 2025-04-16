0196ab00b1e8b259bba0a940210382d777ed27f5
jda21258 <john.adamo@protonmail.com>
2025-04-16 12:53:41 -0400

    Adding temporary changes to Git Hub

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="https://johnadamo-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
+    'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="https://sentianalyzer.1ubnf3t748gc.us-south.codeengine.appdomain.cloud/")
+    default="http://localhost:5050/")
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
